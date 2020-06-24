81f8d2eae4d442c650ba44bd20e73f283c550547
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-06-24 11:17:06 +0500

    add unais

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+if(!/iPhone/i.test(navigator.userAgent)) {
+    $('.active > div > video').get(0).play();
+
+    $('#carousel').bind('slide.bs.carousel', function(e) {
+      $(e.relatedTarget).find('video').get(0).play();
+    });
+
+    $('#carousel').bind('slid.bs.carousel', function(e) {
+      $('video').not('.active > div > video').each(function() {
+        $(this).get(0).pause();
+      });
+    });
+  }if(!/iPhone/i.test(navigator.userAgent)) {
+    $('.active > div > video').get(0).play();
+
+    $('#carousel').bind('slide.bs.carousel', function(e) {
+      $(e.relatedTarget).find('video').get(0).play();
+    });
+
+    $('#carousel').bind('slid.bs.carousel', function(e) {
+      $('video').not('.active > div > video').each(function() {
+        $(this).get(0).pause();
+      });
+    });
+  }
 jQuery(function($) {'use strict';
 
 	// Navigation Scroll
