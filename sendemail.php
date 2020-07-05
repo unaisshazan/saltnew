405c1634b5994f6a04d0c886cc205d4f75597abf
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-05 15:32:47 +0500

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -3,7 +3,7 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'email@email.com';//replace with your email
+$to   		= 'email@email.com';//replace with your email ok
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
